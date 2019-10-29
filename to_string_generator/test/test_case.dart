06261da90c46749c586aeacd2c8a24a1f0a0afbe
YuanJunYao <yuanjunyao@ks-it.co>
2019-10-29 12:03:40 +0800

    Improve test of default settings.

--- to_string_generator/test/test_case.dart
+++ to_string_generator/test/test_case.dart
@@ -1,13 +1,19 @@
-import 'package:source_gen/source_gen.dart';
 import 'package:source_gen_test/annotations.dart';
 import 'package:to_string/to_string.dart';
 
-@ShouldGenerate("""
-String _\$CatToString(Cat o) {
-  return "Cat{color: \${o.color}}";
+@ShouldGenerate(r"""
+String _$PublicFieldIncludeDefaultToString(PublicFieldIncludeDefault o) {
+  return "PublicFieldIncludeDefault{publicField: ${o.publicField}}";
 }
 """)
 @ToString()
-class Cat {
-  var color = "black";
+class PublicFieldIncludeDefault {
+  var publicField;
+  // ignore: unused_field
+  var _privateField;
+  void publicMethod() {}
+  // ignore: unused_element
+  void _privateMethod() {}
+  String get getter => "getter";
+  set setter(String value) {}
 }
