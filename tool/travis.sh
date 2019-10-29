30333d4df6efce55b067a2d0e37533c4d3803d7c
YuanJunYao <yuanjunyao@ks-it.co>
2019-10-29 15:18:44 +0800

    Update travis.sh

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 pushd to_string_generator
+echo 'pub get'
+pub get || exit $?
 echo 'pub run test'
-pub run test || EXIT_CODE=$?
+pub run test || exit $?
 popd
 
 pushd example
+echo 'pub get'
+pub get || exit $?
 echo 'pub run test'
-pub run test || EXIT_CODE=$?
+pub run test || exit $?
 popd
-
-exit ${EXIT_CODE}
