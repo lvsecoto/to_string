cf286da458336035200aa494a72bb285d69d745a
YuanJunYao <yuanjunyao@ks-it.co>
2020-01-14 11:24:30 +0800

    Remove unused test.

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -3,8 +3,5 @@ import 'package:test/test.dart';
 
 void main() {
   test('Test generated toString()', () {
-    var cat = Cat(color: "black", weight: 1.8, wings: null);
-    expect("Cat{classify: Animal, color: black, weight: 1.8, wings: null}",
-        cat.toString());
   });
 }
